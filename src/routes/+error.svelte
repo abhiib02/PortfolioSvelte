608c9037bc5fba0adfefb4c1eb0d690616b091fe
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2023-02-18 21:48:15 +0530

    Added Error Page 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,22 @@
-dasdasdadads
+<script></script>
+<div class="container">
+    <div class="Heading">ERROR</div>
+    <div class="Heading maincolor">404</div>
+</div>
+<style>
+    .container{
+        display:grid;
+        height:100%;
+        place-content:center;
+    }
+    .Heading {
+        font-size: 6rem;
+        font-weight: bold;
+        place-self: center;
+        color: white;
+    }
+    .maincolor {
+        font-size:10rem;
+        color: var(--maincolor);
+    }
+</style>
