d4ea3674460c70471dce462cac15256fd0383e96
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2023-12-20 14:10:12 +0530

    svelte netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter()
+  }
 };
-
 export default config;
